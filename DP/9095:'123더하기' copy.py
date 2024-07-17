e58bb1f7b926a421890196594992d40f73da2383
Chae-Yu <chaeyu2004@naver.com>
2024-07-17 18:54:31 +0900

    boj.9095 123더하기 코드 수정

--- DP/9095:'123더하기' copy.py
+++ DP/9095:'123더하기' copy.py
@@ -13,13 +13,15 @@ for _ in range(T):
     n=int(input())
     
     dp=[0] * (max(4, n+1))
-    dp[0] = 1
-    dp[1] = 2
-    dp[2] = 4
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
     
 
-    for i in range(3,n+1):
+    for i in range(4,n+1):
         dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
-    print(dp[-2])
+    
+    
+    print(dp[n])
     
    
